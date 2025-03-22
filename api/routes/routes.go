e07f4e482574e2eda83ad2d1224cdcaaf51e350b
rrav0016 <rrav0016@student.monash.edu>
2025-03-22 21:26:57 +1100

    updated routes to handle cors

--- api/routes/routes.go
+++ api/routes/routes.go
@@ -4,7 +4,7 @@ import (
 	"database/sql"
 
 	"github.com/Ross1116/gym-tracker-backend/docs"
-	_ "github.com/Ross1116/gym-tracker-backend/docs"
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	swaggerFiles "github.com/swaggo/files"
 	ginSwagger "github.com/swaggo/gin-swagger"
@@ -14,6 +14,14 @@ func SetupRoutes(db *sql.DB) {
 	docs.SwaggerInfo.Host = "localhost:9000"
 
 	router := gin.Default()
+
+	router.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"http://localhost:3000"}, // Your Next.js app URL
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+	}))
 
 	SetupGymRoutes(db, router)
 	SetupEquipmentRoutes(db, router)
